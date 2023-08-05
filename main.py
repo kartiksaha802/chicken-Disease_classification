6e63fd614d93f512c5423234c697ca1245761774
Kartik Saha <kartiksaha802@gmail.com>
2023-08-05 18:58:13 +0530

    flask

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-from cnnClassifier.pipeline.stage_04_training import ModelTrainingPipeline
-from cnnClassifier.pipeline.stage_05_evaluation import EvaluationPipeline
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
